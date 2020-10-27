dd4e342fd3de77a05f6239371b3367320ac16f95
Thomas Nelson <thnlsn@gmail.com>
2020-10-27 09:46:00 -0700

    finished base css, added css custom variables for colors as well as general html layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './css/style.css';
 
 function App() {
-  return <div className='App'>Bookend</div>;
+  return (
+    <div className='container'>
+      <header className='header'>Header</header>
+      <div className='content'>
+        <nav className='sidebar'>Navigation</nav>
+        <main className='hotel-view'>Hotel view</main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
