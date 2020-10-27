5943c36df08d3a119156ad614e2cdd021bae9c30
Thomas Nelson <thnlsn@gmail.com>
2020-10-27 10:21:18 -0700

    created a seperate component for the header to allow it to be reusable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './css/style.css';
 
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import HotelView from './components/MainView';
+
 function App() {
   return (
     <div className='container'>
-      <header className='header'>Header</header>
+      <Header />
       <div className='content'>
         <nav className='sidebar'>Navigation</nav>
         <main className='hotel-view'>Hotel view</main>
