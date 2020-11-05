67180e0b706efb53f28600a3864babc22d9d9b8c
Thomas Nelson <thnlsn@gmail.com>
2020-11-04 20:52:24 -0800

    added some span elements to the button to create a hover effect that will switch between the two

--- src/components/Views/HotelView.js
+++ src/components/Views/HotelView.js
@@ -137,7 +137,10 @@ const HotelView = ({
         <h2 className='cta__book-now'>
           Good news! We have 4 rooms for your selected dates!
         </h2>
-        <button className='btn'>Only 4 rooms left</button>
+        <button className='btn'>
+          <span className='btn__visible'>Book now</span>
+          <span className='btn__invisible'>Only 4 rooms left</span>
+        </button>
       </div>
     </Fragment>
   );
