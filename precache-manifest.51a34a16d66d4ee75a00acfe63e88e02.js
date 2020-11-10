41a02e45da1e6de9668cd6725bbeea126a0b7041
Thomas Nelson <thnlsn@gmail.com>
2020-11-09 22:46:39 -0800

    Updates

---
+++ precache-manifest.51a34a16d66d4ee75a00acfe63e88e02.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0597862e1804e460b11738bb6b133c11",
+    "url": "/bookend/index.html"
+  },
+  {
+    "revision": "04e396028e2a5bc6795d",
+    "url": "/bookend/static/css/main.fcb8cac7.chunk.css"
+  },
+  {
+    "revision": "f1c87595cb0d52f197fd",
+    "url": "/bookend/static/js/2.e06cee72.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/bookend/static/js/2.e06cee72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e396028e2a5bc6795d",
+    "url": "/bookend/static/js/main.d6d12183.chunk.js"
+  },
+  {
+    "revision": "4fb023af65dc74402b84",
+    "url": "/bookend/static/js/runtime-main.71dd85f8.js"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/bookend/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/bookend/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/bookend/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/bookend/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "3203e8f6c24947aa32f9deaaa7b83b67",
+    "url": "/bookend/static/media/sprite.3203e8f6.svg"
+  },
+  {
+    "revision": "4e929d84d78491c5fbff0b73f10d0daa",
+    "url": "/bookend/static/media/user.4e929d84.jpg"
+  }
+]);
